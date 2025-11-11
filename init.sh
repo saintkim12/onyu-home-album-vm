6950db4e28cf9cd02bb6f49c9207c28ea605201d
beancodebox <beancodebox@gmail.com>
2025-11-11 19:14:01 +0900

    수정사항 테스트

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ GIT_USER=saintkim12
 GIT_REPO=onyu-home-album-vm
 GIT_BRANCH=main
 GIT_URL=https://github.com/${GIT_USER}/${GIT_REPO}.git
-MAIN_VM_DIR=/opt/setup/album-vm
+ALBUM_VM_DIR=/opt/setup/album-vm
 YOUR_SERVER_IP='<your-server-ip>'
 
 mkdir -p /opt/setup
@@ -16,60 +16,34 @@ echo "📦 Installing Docker..."
 apk update
 apk add --no-cache docker docker-compose git curl openrc rclone fuse3
 
-# ### [2] rclone.conf 생성
-# CONFIG_DIR="$HOME/.config/rclone"
-# CONFIG_FILE="$CONFIG_DIR/rclone.conf"
+### [2] rclone 초기화
+echo fuse >> /etc/modules # fuse 등록: modprobe fuse
 
-# # ~/.config/rclone 디렉토리가 없으면 생성
-# if [ ! -d "$CONFIG_DIR" ]; then
-#     echo "Creating directory $CONFIG_DIR..."
-#     mkdir -p "$CONFIG_DIR"
-# fi
-
-# # rclone.conf 파일이 없으면 생성
-# if [ ! -f "$CONFIG_FILE" ]; then
-#     echo "Creating rclone config file $CONFIG_FILE..."
-#     cat <<EOF > "$CONFIG_FILE"
-# # ~/.config/rclone/rclone.conf
-# # see http://192.168.1.15:9001/access-keys
-# [immich-s3]
-# type = s3
-# provider = Minio
-# access_key_id = QSvk6zEHUuTNPtdJ9IN0
-# secret_access_key = KDbURVPqGaPAISn96vMQYUI5rQXVgEJLawCYY3gW
-# endpoint = http://192.168.1.15:9000
-# acl = private
-# EOF
-#     echo "rclone.conf has been created."
-# else
-#     echo "$CONFIG_FILE already exists. Skipping creation."
-# fi
-
-# ### [3] immich-s3 마운트
-# MOUNT_POINT="/mnt/immich-s3"
-# # mount point 없으면 생성
-# [ ! -d "$MOUNT_POINT" ] && mkdir -p "$MOUNT_POINT"
-
-
-
+### [3] docker 초기화
 echo "🔌 Enabling docker service..."
 rc-update add docker boot
 service docker start
 
 echo "📥 Cloning Git repository..."
 
-### [2] Git 저장소 클론
-if [ ! -d "$MAIN_VM_DIR" ]; then
-  git clone -b "$GIT_BRANCH" "$GIT_URL" "$MAIN_VM_DIR"
+### [4] Git 저장소 클론
+if [ ! -d "$ALBUM_VM_DIR" ]; then
+  git clone -b "$GIT_BRANCH" "$GIT_URL" "$ALBUM_VM_DIR"
 else
   echo "📦 Repo exists, pulling latest..."
-  cd "$MAIN_VM_DIR" && git pull && cd ..
+  cd "$ALBUM_VM_DIR" && git pull && cd ..
 fi
 
-### [3] 메인 디렉토리로 이동
-cd "$MAIN_VM_DIR"
+### [5] 메인 디렉토리로 이동
+cd "$ALBUM_VM_DIR"
 
-### [4] Portainer Docker 컨테이너 실행
+### [6] rclone 서비스 등록
+cp rclone.conf ~/.config/rclone/rclone.conf # rclone.conf 파일 변경 필요!(key)
+cp rclone.start /etc/local.d/rclone.start
+chmod +x /etc/local.d/rclone.start
+rc-update add local
+
+### [7] Portainer Docker 컨테이너 실행
 echo "🚀 Starting Portainer..."
 cd portainer
 export PORTAINER_PORT=8100
@@ -82,4 +56,3 @@ echo ""
 echo "📝 Next Step:"
 echo "1. Open Portainer UI"
 echo "2. Set Stack and Run Container"
-
--- restart.sh
+++ restart.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 YOUR_SERVER_IP='<your-server-ip>'
-DEV_VM_DIR=/opt/setup/dev-vm
-echo "🔁 Restarting Docker + Portainer Agent..."
+MAIN_VM_DIR=/opt/setup/album-vm
+export PORTAINER_PORT=8100
+echo "🔁 Restarting Docker + Portainer..."
 
 service docker restart
 
-cd "$DEV_VM_DIR/portainer"
+cd "$MAIN_VM_DIR/portainer"
 docker-compose down
 docker-compose up -d
 
 echo "✅ Portainer restarted on :$PORTAINER_PORT"
 echo "👉 Access at: http://$YOUR_SERVER_IP:$PORTAINER_PORT"
-
-cd "$DEV_VM_DIR/portainer-agent"
-docker compose down
-docker compose up -d
-
-echo "✅ Portainer Agent restarted on :$PORTAINER_AGENT_PORT"
